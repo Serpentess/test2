ec59e23e0f8dcb8a855443b3bfc1982722fb2843
Валерія Герасименко <V.Herasymenko_FIT_11_21_B_d@knute.edu.ua>
2022-11-07 22:43:14 +0200

    .

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        class Converter
+        {
+
+            double hrn;
+            public void SetHrn(double value)
+            {
+                hrn = value;
+            }
+            public double GetHrn()
+            {
+                return hrn;
+            }
+            double usd;
+            public double GetUsd()
+            {
+                return usd;
+            }
+            double eur;
+            public double GetEur()
+            {
+                return eur;
+            }
+            double pln;
+            public double GetPln()
+            {
+                return pln;
+            }
+            public double nUsd;
+            public void SetNUsd(double value)
+            {
+                nUsd = value;
+            }
+            public double GetNUsd()
+            {
+                return nUsd;
+            }
+            public double MethodUsd(double usd, double hrn, double nUsd)
+            {
+                nUsd = usd * hrn;
+                return nUsd;
+            }
+            public double MethodEur(double eur, double hrn, double nEur)
+            {
+                nEur = eur * hrn;
+                return nEur;
+            }
+            public double MethodPln(double pln, double hrn, double nPln)
+            {
+                nPln = pln * hrn;
+                return nPln;
+            }
+            public Converter(double usd, double eur, double pln, double hrn)
+            {
+                this.usd = usd;
+                this.eur = eur;
+                this.pln = pln;
+                Console.Write("Введiть суму(грн):");
+                this.hrn = Convert.ToDouble(Console.ReadLine());
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Converter money = new Converter(40.62, 42.61, 7.89, 4);
+            Console.WriteLine("Курси валют: ");
+            Console.WriteLine("Долар: {0}, \nЕвро: {1}, \nЗлоти: {2}, \nВведена сума: {3}", money.GetUsd(), money.GetEur(), money.GetPln(), money.GetHrn());
+            money.MethodUsd(40.62, money.GetHrn(), 1);
+            Console.WriteLine(money.MethodUsd(40.62, money.GetHrn(), 1));
+            money.MethodEur(42.50, money.GetHrn(), 1);
+            Console.WriteLine(money.MethodEur(42.61, money.GetHrn(), 1));
+            money.MethodPln(7.89, money.GetHrn(), 1);
+            Console.WriteLine(money.MethodPln(7.89, money.GetHrn(), 1));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,85 @@
+﻿class Employee
+{
+    public string name; 
+    public string surname; 
+    public static string? dateOfHire; 
+    public OperateCost operationCost;
+    public Employee(string name, string surname, string dateOfHire, OperateCost opCost)
+    {
+        this.name = name;
+        this.surname = surname;
+        Employee.dateOfHire = dateOfHire;
+        this.operationCost = opCost;
+
+    }
+    public static double DiscoverGrade(string dateOfHire)
+    {
+        double dateValueForGrade = (DateTime.Now - DateTime.Parse(dateOfHire)).TotalDays;
+
+        if (dateValueForGrade >= 1825 && dateValueForGrade < 3650)
+            return 1.1;
+        else if (dateValueForGrade >= 3650)
+            return 1.25;
+        else
+            return 1.5;
+    }
+}
+
+public abstract class OperateCost
+{
+    public double salary;
+    public double tax;
+
+    public abstract void ApplyBonus(double bonus, double grade);
+    public abstract void ApplyTax();
+}
+
+class Slave : OperateCost
+{
+    public override void ApplyBonus(double bonus, double grade)
+    {
+        salary = (1000 + 2 * bonus) * grade;
+    }
+
+    public override void ApplyTax()
+    {
+        tax = salary * 0.34;
+    }
+}
+class вчитель : OperateCost
+{
+    public override void ApplyBonus(double bonus, double grade)
+    {
+        salary = (1000 + 5 * bonus) * grade;
+    }
+    public override void ApplyTax()
+    {
+        tax = salary * 0.34;
+    }
+}
+
+class Lord : OperateCost
+{
+    public override void ApplyBonus(double bonus, double grade)
+    {
+        salary = (1000 + 10 * bonus) * grade;
+    }
+    public override void ApplyTax()
+    {
+        tax = salary * 0.34;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        OperateCost oc = new вчитель();
+        Employee emp = new Employee("Володимр", "Пасів", "06.09.2019", oc);
+        double grade = Employee.DiscoverGrade("06.09.2019");
+        Console.WriteLine("Ім'я: {0}, прізвище: {1}, дата прийняття на роботу: {2}, посада: {3}", emp.name, emp.surname, Employee.dateOfHire, emp.operationCost);
+        oc.ApplyBonus(2500, grade);
+        oc.ApplyTax();
+        Console.WriteLine("Salary: {0}, Tax: {1}", emp.operationCost.salary, emp.operationCost.tax);
+        Console.ReadKey();
+    }
+}
